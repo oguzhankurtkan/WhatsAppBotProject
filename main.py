d2685c98e6f7d550423b9e591736e34e1769bc58
Oğuzhan Kurtkan <oguzhankurtkan@gmail.com>
2024-12-20 11:05:04 +0300

    textlist added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 import requests
 
-with open ("text.txt", "r", encoding="utf-8"):
+with open ("text.txt", "r", encoding="utf-8") as text:
+    textlist = list()
+    message = text.read()
+    # To make it read each line separately
+    textlist = message.split("\n")
+
+print (textlist)
