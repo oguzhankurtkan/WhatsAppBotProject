106b483844b96af5aa1ba4edf37ea8b66d5bf46d
Oğuzhan Kurtkan <oguzhankurtkan@gmail.com>
2024-12-21 17:33:52 +0300

    driver added

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ with open ("text.txt", "r", encoding="utf-8") as text:
     # To make it read each line separately
     textlist = message.split("\n")
 
-print (textlist)
+def start():
+    # Using Chrome and 4 seconds wait
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(4)
